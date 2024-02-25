ea7d9d0039a708373e8e80697f1afa94cbede61b
juanjopl <jjplo2019@gmail.com>
2024-02-25 03:12:33 +0100

    He quitado la practica de tema 3 y 4

--- contenido/Práctica Tema 3 y 4/src/componentes/Error.jsx
+++
@@ -1,8 +0,0 @@
-function Error() {
-    return(
-        <header>
-            <h2>404 Página no encontrada</h2>
-        </header>
-    )
-}
-export default Error;
--- contenido/Práctica Tema 3 y 4/src/componentes/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-        <div className="footer">
-            <p>&copy; 2024 Tu Empresa. Todos los derechos reservados.</p>
-        </div>
-    )
-}
-export default Footer;
